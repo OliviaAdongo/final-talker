69313959d1ecdcaba99e59425555ce71dcd8536f
Olivia Adongo <akinyioliviatove@gmail.com>
2022-07-31 18:36:58 +0300

    Connection established between front-end and backend

--- server/index.js
+++ server/index.js
@@ -4,5 +4,24 @@ const app = express();
 const cors = require ('cors');
 
 app.use(cors());
+app.use(express.json());
 
-const server = app.listen('3002', ()=> console.log('Server running on port 3002!!!'))
+
+
+
+const server = app.listen('3002', ()=> console.log('Server running on port 3002!!!'));
+
+io = socket(server);
+
+io.on('connection', (socket)=>{
+    console.log(socket.id)// Random id given to whoever logs into the application
+    //data represents the room we are creating  by receiving data and creating that room
+    socket.on('join_room', (data)=>{
+        socket.join(data)
+        console.log('User joined room' + data);
+    });
+
+    socket.on('disconnect', ()=> {
+        console.log('USER DISCONNECTED')
+    })
+    });
